e60ecec630093aa255dd638cd1e147a3f50e210f
Matheus Popolin <110188966+MatheusPopolin@users.noreply.github.com>
2023-05-03 15:35:38 -0300

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from .models import User
 
-# werwerwer
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
