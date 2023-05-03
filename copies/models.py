590ae4be4fe87e909b03a2051a9886db8913a3be
Matheus Popolin <matheuspopolin@hotmail.com>
2023-05-03 14:09:55 -0300

    feat: create relation books/copies

--- copies/models.py
+++ copies/models.py
@@ -3,3 +3,9 @@ from django.db import models
 
 class Copie(models.Model):
     is_avaliable = models.BooleanField(default=True)
+
+    book = models.ForeignKey(
+        "books.Book",
+        on_delete=models.CASCADE,
+        related_name="copies",
+    )
