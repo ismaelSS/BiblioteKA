198c051e1cfd87e38cb3f923d4d14b1de77848c8
unknown <ismael1337sn@gmail.com>
2023-05-04 01:16:22 -0300

    feat:create users view

--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 from .models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer
-from utils.permissions import IsAccountOwner
+from utils.permissions import IsAccountOwner, IsAdminUser
 from rest_framework import generics
 
 
 class UserView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
--- utils/permissions.py
+++ utils/permissions.py
@@ -6,3 +6,7 @@ from rest_framework.views import View
 class IsAccountOwner(permissions.BasePermission):
     def has_object_permission(self, request, view: View, obj: User) -> bool:
         return request.user.is_authenticated and obj == request.user
+
+class IsAdminUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_admin
