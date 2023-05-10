8cb84f3124ff839a5fdbccbe2670435479253f6b
ismaelSS <104331841+ismaelSS@users.noreply.github.com>
2023-05-10 20:28:48 -0300

    fix: min max page numbers

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Book(models.Model):
@@ -7,7 +8,7 @@ class Book(models.Model):
 
     title = models.TextField(max_length=100)
     author = models.TextField(max_length=100)
-    pages = models.IntegerField(null=True, blank=True)
+    pages = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(50560)])
     afterword = models.TextField(null=True, blank=True)
     publisher = models.TextField(max_length=100, null=True, blank=True)
     publication_date = models.DateField(null=True, blank=True)
